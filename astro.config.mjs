f4f8fa45a8697a9024e504d7defdd33c91a7d590
Drix <mcorrochanoarroyo@gmail.com>
2025-06-10 16:05:29 +0200

    feat: add tailwind & fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config'
+import { defineConfig, fontProviders } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+	vite: {
+		plugins: [tailwindcss()]
+	},
+	experimental: {
+		fonts: [
+			{
+				cssVariable: '--font-dm-sans',
+				name: 'DM Sans',
+				provider: fontProviders.fontsource()
+			}
+		]
+	}
+})
