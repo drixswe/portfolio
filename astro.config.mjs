2119f3b10901866ad4e225372085af6f01a3a64e
Drix <mcorrochanoarroyo@gmail.com>
2025-06-12 11:26:52 +0200

    feat: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig, fontProviders } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
+import vercel from '@astrojs/vercel'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://drix.one',
+  adapter: vercel(),
 	vite: {
 		plugins: [tailwindcss()]
 	},
